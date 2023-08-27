70c241b8ee57638130c07f476f61cf70a1b682fb
Anderson-Junior <ander.lemos.jr@gmail.com>
2023-08-27 11:14:02 -0300

    novas implementacoes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilters } from './exceptions-filters/prisma.exception-filters';
+import { CatchAllErrorsExceptionsFilter } from './exceptions-filters/catch-all-errors.exceptions-filter';
+import { ValidationPipe } from '@nestjs/common';
+import { InvalidRelationExcetionFilter } from './exceptions-filters/invalid-relation.exception-filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalFilters(new PrismaExceptionFilters());
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new CatchAllErrorsExceptionsFilter(httpAdapter),
+    new PrismaExceptionFilters(),
+    new InvalidRelationExcetionFilter(),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs 10 - Video API')
+    .setDescription('The video API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
